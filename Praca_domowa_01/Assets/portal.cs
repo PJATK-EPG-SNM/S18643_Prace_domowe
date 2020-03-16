1a45d5e0e47ab12365bba90c2799bcd52239a27d
s18643-pja <61684678+s18643-pja@users.noreply.github.com>
2020-03-16 21:03:24 +0100

    v0.02
    Praca domowa, potato mode

---
+++ Praca_domowa_01/Assets/portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+
+    public GameObject portal1;
+
+    public GameObject ludek;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ludek")
+        {
+
+        }
+            }
+    
+}
