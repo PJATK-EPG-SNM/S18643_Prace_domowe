b5ebd90bbd5d13d81f22e3b399fc3b84f3ec7b71
s18643-pja <61684678+s18643-pja@users.noreply.github.com>
2020-03-24 16:57:49 +0100

    v0.003
    Praca domowa 02

---
+++ Praca domowa_02/Assets/gfx/ludekSterowanie.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ludekSterowanie : MonoBehaviour
+{
+    public float speed = 100;
+    public float jump = 550.3f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown("space") && (isGrounded == true))
+        {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+        }
+
+    }
+   // void OnCollisionEnter2D(Collision2D col)
+   // {
+
+   //     if (col.gameObject.name.Equals("portal1"))
+    //        this.transform.parent = col.transform;
+    //    isGrounded = true;
+    //    if (col.gameObject.name.Equals("lewpraw"))
+    //        this.transform.parent = col.transform; 
+
+
+   // }
+   //   void OnCollisionExit2D(Collision2D col)
+   // {
+   //     if (col.gameObject.name.Equals("portal1"))
+   //         this.transform.parent = null;
+
+   //     if (col.gameObject.name.Equals("lewpraw"))
+   //         this.transform.parent = null;
+   // }
+}
