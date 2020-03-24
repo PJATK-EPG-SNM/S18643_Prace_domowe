67f15feaa88f6b7f697308903de1df7a74c205b9
s18643-pja <61684678+s18643-pja@users.noreply.github.com>
2020-03-24 16:57:19 +0100

    v0.02

---
+++ Praca_domowa_01/Assets/assets/gfx/platformaSpada.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformaSpada : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals ("ludek"))
+        {
+            Invoke("Spada", 1f);
+            Destroy(gameObject, 1.6f);
+        }
+    }
+    void Spada()
+    {
+        rb.isKinematic = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Praca_domowa_01/Assets/assets/gfx/prawoLewo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prawoLewo : MonoBehaviour
+{
+    float dirX, moveSpeed = 2f;
+    bool moveRight = true;
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 3.5f)
+            moveRight = false;
+        if (transform.position.x < -1.5f)
+            moveRight = true;
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+}
